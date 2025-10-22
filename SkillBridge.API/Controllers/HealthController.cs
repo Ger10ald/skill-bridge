9de28036e56a0ea4f038f40599406c3b47ebfcd0
Gerald Dukuly <gerald101201@gmail.com>
2025-10-21 19:42:01 -0500

    Added /health endpoint for Azure deployment test

---
+++ SkillBridge.API/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SkillBridge.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase   
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "SkillBridge OK", timestamp = DateTime.UtcNow });
+        }
+    }
+}
